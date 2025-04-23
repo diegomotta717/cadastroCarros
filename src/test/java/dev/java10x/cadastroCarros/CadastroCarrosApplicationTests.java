d725750eddb5b330b2c51f329f36f847c0e0220c
unknown <diegomotta717@gmail.com>
2025-04-23 13:40:45 -0300

    first commit

---
+++ src/test/java/dev/java10x/cadastroCarros/CadastroCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastroCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
